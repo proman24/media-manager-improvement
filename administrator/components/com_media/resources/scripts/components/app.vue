601a3cd454f423118f77293716947428fd3fe6cb
Geetanshu <mathur.geetanshu887@gmail.com>
2018-06-02 19:30:42 +0530

    adding delete modal for toolbar

--- administrator/components/com_media/resources/scripts/components/app.vue
+++ administrator/components/com_media/resources/scripts/components/app.vue
@@ -45,7 +45,11 @@
         created() {
             // Listen to the toolbar events
             MediaManager.Event.listen('onClickCreateFolder', () => this.$store.commit(types.SHOW_CREATE_FOLDER_MODAL));
-            MediaManager.Event.listen('onClickDelete', () => this.$store.dispatch('deleteSelectedItems'));
+            MediaManager.Event.listen('onClickDelete', () => {
+                //this.$store.dispatch('deleteSelectedItems')
+                this.$store.commit(types.SELECT_BROWSER_ITEM, this.item);
+                this.$store.commit(types.SHOW_CONFIRM_DELETE_MODAL);
+            });
         },
         mounted() {
             // Set the full height and add event listener when dom is updated
